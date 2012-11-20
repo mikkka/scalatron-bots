47ddcd58a78685b33b26b50c94d6d64972e29e21
mick <mick@z-inc.net>
2012-11-20 11:42:04 +0600

    - еще рефакторинг

--- n7/src/main/scala/Bot.scala
+++ n7/src/main/scala/Bot.scala
@@ -21,7 +21,17 @@ object ControlFunction {
 }
 
 object BotStrategies {
-  def weigthElRelative(elxy: ElementXY) = elxy.el match {
+  def eatAndRunWeights(elxy: ElementXY) = elxy.el match {
+    case Snorg => sqrWeight(-400.0, elxy.xy.length, 1, 100)
+    case Toxifera => sqrtWeight(-10.0, elxy.xy.length, 0.1, 100)
+    case Empty => sqrtWeight(1.0, elxy.xy.length, 0.1, 100)
+    case Wall => sqrWeight(-10.0, elxy.xy.length, 0.1, 10000)
+    case Fluppet => linearWeight(400.0, elxy.xy.length, 0.1, 100)
+    case Zugar => linearWeight(200.0, elxy.xy.length, 0.1, 100)
+    case _ => 0.0
+  }
+
+  def goHomeWeights(elxy: ElementXY) = elxy.el match {
     case Snorg => sqrWeight(-400.0, elxy.xy.length, 1, 100)
     case Toxifera => sqrtWeight(-10.0, elxy.xy.length, 0.1, 100)
     case Empty => sqrtWeight(1.0, elxy.xy.length, 0.1, 100)
@@ -38,17 +48,18 @@ object BotStrategies {
     case _ => false
   }
 
-  def weightPos(view: View, pos: XY, vector: XY) = view.offsets(pos + vector, _ != Unknown).foldLeft(0.0) {(accu, exy) =>
-    val cos = vector.scalar(exy.xy) / (vector.length * exy.xy.length)
-    accu +  weigthElRelative(exy) * (if (cos > 0.8) cos else 0.8)
-  }
+  def weightPos(view: View, pos: XY, vector: XY, weights: ElementXY => Double) =
+    view.offsets(pos + vector, _ != Unknown).foldLeft(0.0) {(accu, exy) =>
+      val cos = vector.scalar(exy.xy) / (vector.length * exy.xy.length)
+      accu +  weights(exy) * (if (cos > 0.8) cos else 0.8)
+    }
 
-  def eatAndRun(input: Input, output: Output) = {
+  def makeMove(input: Input, output: Output, weights: ElementXY => Double) = {
     val view = input.view
 
     val movesWeights = XY.directions.
       filter(xy => !donttouchit(view.from(xy))).
-      map(xy => (xy, weightPos(view, view.center, xy)))
+      map(xy => (xy, weightPos(view, view.center, xy, weights)))
 
     if (!movesWeights.isEmpty) {
       val currCoord = input.coords.head
@@ -85,11 +96,11 @@ object BotStrategies {
   }
 
   def eatRunLove(input: Input): Output = {
-    makeLove(input, eatAndRun(input, new Output))
+    makeLove(input, makeMove(input, new Output, eatAndRunWeights))
   }
 
   def goHome(input: Input): Output = {
-    new Output()
+    makeLove(input, makeMove(input, new Output, goHomeWeights))
   }
 
   def react(input: Input): Output = {
