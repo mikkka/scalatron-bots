c0f7214e80bd265505500a6c4488973a91b74cbb
mick <mick@z-inc.net>
2012-12-25 00:04:09 +0600

    -  бесполезные константы

--- n7/src/main/scala/XY.scala
+++ n7/src/main/scala/XY.scala
@@ -23,4 +23,15 @@ object XY {
 
   val Zero = XY(0, 0)
   val directions = (for (x <- -1 to 1; y <- -1 to 1 if !(x == 0 && y == 0)) yield XY(x, y)).toArray
+  val topRight = List(
+    XY(-1,-1), XY(0,-1), XY(1,-1),
+                         XY(1, 0)
+  )
+
+  val collisionScanMask = List(
+    XY(-2,-2), XY(-1,-2), XY(0,-2), XY(1,-2), XY(2,-2),
+    XY(-2,-1), XY(-1,-1), XY(0,-1), XY(1,-1), XY(2,-1),
+    XY(-2, 0), XY(-1, 0),           XY(1, 0), XY(2, 0),
+                          XY(0, 1), XY(1, 1), XY(2, 1)
+  )
 }
