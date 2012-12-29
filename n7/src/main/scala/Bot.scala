8b6bc8b826414cc973ee2a6b96f72e4a9a85d490
mick <mick@z-inc.net>
2012-12-29 16:48:20 +0600

    - подрыв в конце

--- n7/src/main/scala/Bot.scala
+++ n7/src/main/scala/Bot.scala
@@ -208,8 +208,8 @@ object BotStrategies {
     val generation = input.generation
 
     if (generation == 0) master(input, out)
+    else if (input.time > 4990) out.append(Explode(7)).say("watafaka!")
     else if (input.time > 4900) aggressiveGoHome(input, out)
-    else if (input.time > 4995) out.append(Explode(3)).say("watafaka!")
     else if (energy > 1000) goHome(input, out)
     else if (mood == "shahid") aggressive(input, out)
     else if (mood == "feeder" && energy > 1000) aggressiveGoHome(input, out)
